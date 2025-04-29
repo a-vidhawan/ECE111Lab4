8a9c5f695879cd4f5c8e9d8d3d65dffe4113330b
a-vidhawan <aarav@printsales.in>
2025-04-29 00:03:40 -0700

    added new file and implemented mux

--- Homework 4/mux_2x1_behavioral.sv
+++ Homework 4/mux_2x1_behavioral.sv
@@ -7,6 +7,15 @@ module mux_2x1(
 
 // fill in the guts
 //  pseudocode: out =sel? in1 : in0;
+
+always_comb begin
+  if (sel) begin
+    out = in1;
+  end else begin
+    out = in0;
+  end
+end
+
 endmodule
  
 
